22b6ecba80c4cf6d1eb00d59da4f3ea404b7047d
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-06-08 23:02:07 +0100

    TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that uses TypeVar to annotate it's
+parameter
+"""
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping[Any, T],
+        key: Any, default: T) -> Union[Any, T]:
+    """
+    Safely gets a value from a dictionary by key,
+    returning a default value if the key is not found.
+
+    Args:
+        dct (Dict[Any, T]): The dictionary from which to get the value.
+        key (Any): The key to look for in the dictionary.
+        default (Optional[T], optional): The default value to return
+        if the key is not found. Defaults to None.
+
+    Returns:
+        Optional[T]: The value from the dictionary if the key is found,
+        otherwise the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
