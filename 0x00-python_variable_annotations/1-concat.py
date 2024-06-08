da933d45999deddcf845fc5fd2a42fe008143e19
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-06-08 13:08:34 +0100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This is a module that contains a type-annotated function concat
+that takes a string str1 and a string str2 as arguments and
+returns a concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result
+    """
+    return str1 + str2
